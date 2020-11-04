817e878293b1910a37fb6db7ff959616cbf03bde
Peter Bell <peter@pbell.com>
2020-11-04 11:35:50 -0700

    Added pages feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'auth/auth0/callback', to: 'auth0#callback'
   get 'auth/failure', to: 'auth0#failure'
   get 'logout', to: 'logout#logout'
+  get 'about', to: 'pages#about'
 
   namespace :admin do
     root to: 'events#index'
