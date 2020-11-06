9e72bcc83b824d6315ab4fc0360e5f1a165e890d
Pavlo Semehen <semegen2010@gmail.com>
2020-11-06 17:23:13 +0200

    removes extra controllers, adds authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'dashboard', to: 'dashboard#show'
+  get 'about', to: 'pages#about'
+  get 'dashboard', to: 'pages#dashboard'
   get 'auth/auth0/callback', to: 'auth0#callback'
   get 'auth/failure', to: 'auth0#failure'
-  get 'logout', to: 'logout#logout'
-  get 'about', to: 'pages#about'
+  get 'logout', to: 'auth0#logout'
 
   namespace :admin do
     root to: 'events#index'
